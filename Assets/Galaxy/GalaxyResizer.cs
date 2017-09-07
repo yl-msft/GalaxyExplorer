437eaeed26607294725c9096c249d6e2c4dabc76
Tim Gerken <timg@microsoft.com>
2017-09-06 17:30:36 -0700

    Disable CursorCollisionBackground if alternate gaze ray is in use.

--- Assets/Galaxy/GalaxyResizer.cs
+++ Assets/Galaxy/GalaxyResizer.cs
@@ -1,18 +1,18 @@
 ﻿// Copyright Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using GalaxyExplorer.HoloToolkit.Unity;
+using UnityEngine;
 
 namespace GalaxyExplorer
 {
     public class GalaxyResizer : GE_Singleton<GalaxyResizer>
     {
+        private BoxCollider cursorCollisionBackground = null;
+
         void Awake()
         {
             transform.localScale = transform.localScale * MyAppPlatformManager.GalaxyScaleFactor;
-        }
-        void Start()
-        {
+
             SpiralGalaxy[] spirals = GetComponentsInChildren<SpiralGalaxy>();
             foreach (var spiral in spirals)
             {
@@ -23,8 +23,24 @@ namespace GalaxyExplorer
                 }
             }
 
+            // disable the script if we aren't on an immersive HMD
+            // if we are, we want update to be called so we can do some layer adjusting...
+            enabled = (MyAppPlatformManager.Platform == MyAppPlatformManager.PlatformId.ImmersiveHMD);
+            if (enabled)
+            {
+                cursorCollisionBackground = GetComponentInChildren<BoxCollider>();
+                if (cursorCollisionBackground == null ||
+                    !cursorCollisionBackground.gameObject.name.Equals("CursorCollisionBackground"))
+                {
+                    Debug.Log("Couldn't find CursorCollisionBackground...");
+                    enabled = false;
+                }
+            }
         }
+
         void Update()
-        { }
+        {
+            cursorCollisionBackground.enabled = !MotionControllerInput.Instance.UseAlternateGazeRay;
+        }
     }
 }
