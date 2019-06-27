ec55347dc76540d9f78c9f590fbc8af5c4bf8c94
Ben Turner <ben.turner@microsoft.com>
2019-06-27 11:57:28 +0200

    [15803] Hand menu onboarding

---
+++ Assets/scripts/SceneGameObjectEnabler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GalaxyExplorer;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneGameObjectEnabler : MonoBehaviour
+{
+
+    private static Dictionary<string, int> seenScenes = new Dictionary<string, int>();
+    
+    [SerializeField] private float delay;
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private int executeOnLoadNumber;
+
+    private IEnumerator Start()
+    {
+        yield return IsInTransitionRoutine();
+        yield return new WaitForSeconds(delay);
+        var sceneName = gameObject.scene.name;
+        if (seenScenes.ContainsKey(sceneName))
+        {
+            seenScenes[sceneName]++;
+        }
+        else
+        {
+            seenScenes.Add(sceneName,0);
+        }
+        targetObject.SetActive(seenScenes[sceneName] == executeOnLoadNumber || executeOnLoadNumber < 0);
+    }
+
+    private IEnumerator IsInTransitionRoutine()
+    {
+        while (GalaxyExplorerManager.Instance.TransitionManager.InTransition)
+        {
+            yield return null;
+        }
+    }
+}
